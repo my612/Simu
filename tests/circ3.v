a8825cd5d85c8509036025fd22d067dd693d6085
Ebtsam <ebtsamzyada@aucegypt.edu>
2024-10-16 20:54:46 +0300

    circuit 3 done (2x4 decoder)

--- tests/circ3.v
+++ tests/circ3.v
@@ -9,6 +9,6 @@ module circ3(input [1:0] in, output[3:0] out, input en);
   and (out[0], w[1], w[0], en);
   and (out[1], w[1], in[0], en);
   and (out[2], in[1], w[0], en);
-  and (out[3], in[1], in[0], en); 
+  and (out[3], in[1], in[0], en);
   
 endmodule
