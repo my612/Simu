6ff8bd59e0c170e619fb777b6af2676ff9c7e0eb
my612 <muhammadyasser@aucegypt.edu>
2024-10-22 10:17:49 +0300

    reading from the verilog files done

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import time
 
 class NOT:
-    def __init__(self, inpName, input, outName, delay=0):
-        self.input = input
+    def __init__(self, inpName, outName, delay=0, gname=""):
+        self.gate_name = gname
+        self.input = 0
         self.input_name = inpName
         self.output_name = outName
         self.delay = delay
     def value(self):
         time.sleep(self.delay)
         return not self.input
-    
+    def setInput(self, input):
+        self.input = input
         
 
 class AND:
-    def __init__(self, inpName1, inpName2, input1, input2, outName, delay=0):
-        self.input1 = input1
-        self.input2 = input2
+    def __init__(self, inpName1, inpName2, outName, delay=0, gname=""):
+        self.gate_name = gname
+        self.input1 = 0
+        self.input2 = 0
         self.input_name1 = inpName1
         self.input_name2 = inpName2
         self.output_name = outName
@@ -24,11 +27,15 @@ class AND:
     def value(self):
         time.sleep(self.delay)
         return self.input1 and self.input2
-
-class OR:
-    def __init__(self, inpName1, inpName2, input1, input2, outName, delay=0):
+    def setInputs(self, input1, input2):
         self.input1 = input1
         self.input2 = input2
+
+class OR:
+    def __init__(self, inpName1, inpName2, outName, delay=0, gname=""):
+        self.gate_name = gname
+        self.input1 = 0
+        self.input2 = 0
         self.input_name1 = inpName1
         self.input_name2 = inpName2
         self.output_name = outName
@@ -37,11 +44,15 @@ class OR:
     def value(self):
         time.sleep(self.delay)
         return self.input1 or self.input2
-    
-class NAND:
-    def __init__(self, inpName1, inpName2, input1, input2, outName, delay=0):
+    def setInputs(self, input1, input2):
         self.input1 = input1
         self.input2 = input2
+        
+class NAND:
+    def __init__(self, inpName1, inpName2, outName, delay=0, gname=""):
+        self.gate_name = gname
+        self.input1 = 0
+        self.input2 = 0
         self.input_name1 = inpName1
         self.input_name2 = inpName2
         self.output_name = outName
@@ -50,11 +61,15 @@ class NAND:
     def value(self):
         time.sleep(self.delay)
         return not(self.input1 and self.input2)
-
-class NOR:
-    def __init__(self, inpName1, inpName2, input1, input2, outName, delay=0):
+    def setInputs(self, input1, input2):
         self.input1 = input1
         self.input2 = input2
+        
+class NOR:
+    def __init__(self, inpName1, inpName2, outName, delay=0 , gname=""):
+        self.gate_name = gname
+        self.input1 = 0
+        self.input2 = 0
         self.input_name1 = inpName1
         self.input_name2 = inpName2
         self.output_name = outName
@@ -63,11 +78,15 @@ class NOR:
     def value(self):
         time.sleep(self.delay)
         return not(self.input1 or self.input2)
-    
-class XOR:  
-    def __init__(self, inpName1, inpName2, input1, input2, outName, delay=0):
+    def setInputs(self, input1, input2):
         self.input1 = input1
         self.input2 = input2
+    
+class XOR:  
+    def __init__(self, inpName1, inpName2, outName, delay=0 , gname=""):
+        self.gate_name = gname
+        self.input1 = 0
+        self.input2 = 0
         self.input_name1 = inpName1
         self.input_name2 = inpName2
         self.output_name = outName
@@ -76,10 +95,14 @@ class XOR:
     def value(self):
         time.sleep(self.delay)
         return self.input1 ^ self.input2
+    def setInputs(self, input1, input2):
+        self.input1 = input1
+        self.input2 = input2
     
 class buffer:
-    def __init__(self, inpName, input, outName, delay=0):
-        self.input = input
+    def __init__(self, inpName, outName, delay=0 , gname=""):
+        self.gate_name = gname
+        self.input = 0
         self.input_name = inpName
         self.output_name = outName
         self.delay = delay
@@ -87,53 +110,64 @@ class buffer:
     def value(self):
         time.sleep(self.delay)
         return self.input   
+    def setInput(self, input):
+        self.input = input
 
 
 def parseVerilog(filePath):
     
-    f = open(filePath, "r")
     inputs = []
     outputs = []
     wires = []
+    gates = []
+    with open(filePath, 'r') as f:
+        for line in f:
+            line = line.replace(";", "")
+            line = line.split()
+            if(line):
+                if(line[0] == "input"):
+                    inputs.append(line[1])
+                if(line[0] == "output"):
+                    outputs.append(line[1])
+                if(line[0] == "wire"):
+                    wires.append(line[1])
+                if(line[0] == "not"):
+                    parameters = line[3].strip("();").split(",")
+                    gates.append(NOT(parameters[1], parameters[0], int(line[1].strip("#()")), line[2]))
+                if(line[0] == "and"):
+                    parameters = line[3].strip("();").split(",")                    
+                    gates.append(AND(parameters[2], parameters[1], parameters[0], int(line[1].strip("#()")), line[2]))
+                if(line[0] == "or"):
+                    parameters = line[3].strip("();").split(",")                    
+                    gates.append(OR(parameters[2], parameters[1], parameters[0], int(line[1].strip("#()")), line[2]))
+                if(line[0] == "nand"):
+                    parameters = line[3].strip("();").split(",")                    
+                    gates.append(NAND(parameters[2], parameters[1], parameters[0], int(line[1].strip("#()")), line[2]))
+                if(line[0] == "xor"):
+                    parameters = line[3].strip("();").split(",")                    
+                    gates.append(XOR(parameters[2], parameters[1], parameters[0], int(line[1].strip("#()")), line[2]))
+                if(line[0] == "NOR"):
+                    parameters = line[3].strip("();").split(",")                    
+                    gates.append(NOR(parameters[2], parameters[1], parameters[0], int(line[1].strip("#()")), line[2]))
+
+        return inputs, outputs, gates
     
-    for x in f:
-        line = f.split()
-        if(line[0] == "input"):
-            inputs.append(line[1])
-        if(line[0] == "output"):
-            outputs.append(line[1])
-        
 
-    
-parseVerilog("test.v")
-
-class NOT:
-    def __init__(self, inpName, input, outName, delay):
-        self.input = input
-        self.input_name = inpName
-        self.output_name = outName
-        self.delay = delay
-    def value():
-        return not input
-    
-        
-
-
-def parseVerilog(filePath):
-    f = open(filePath, "r")
-    inputs = {}
-    outputs = {}
-    wires = {}
-    gates = {}
-    for x in f:
-        line = f.split()
-        if(line[0] == "input"):
-            inputs.append(line[1])
-        if(line[0] == "output"):
-            outputs.append(line[1])
-        if(line[0] == "wire"):
-            wires.append(line[1])
             
-    return inputs, outputs, wires
-    
-ins, outs, ws = parseVerilog("test.v")
+# and #(5) g0 (w,a,b);
+        
+# not #(2) g1 (n,a);
+        
+# or #(9) g2 (y,n,c);
+        
+# nand #(7) g3 (z,y,w);
+        
+
+ins, outs, gates = parseVerilog("./tests/circ1.v")
+print (" =--=-=-=-=--= ")
+print(ins)
+print("_________________")
+print(outs)
+print("_________________")
+for g in gates:
+    print(g.gate_name)
--- tests/circ1.v
+++ tests/circ1.v
@@ -21,22 +21,22 @@ wire w8;
 wire sel_not0;
 wire sel_not1;
 
-not #(2) g1(sel_not0, sel0);
-not #(2) g2(sel_not1, sel1);
+not #(2) g1 (sel_not0,sel0);
+not #(2) g2 (sel_not1,sel1);
 
 
-and #(5) g3(w1, sel_not1, sel_not0);
-and #(5) g4(w2, w1, in0); //W2
+and #(5) g3 (w1,sel_not1,sel_not0);
+and #(5) g4 (w2,w1,in0); //W2
 
 
-and #(5) g5(w3, sel_not1, sel0);
-and #(5) g6(w4, w3, in1); //W4
+and #(5) g5 (w3,sel_not1,sel0);
+and #(5) g6 (w4,w3,in1); //W4
 
 
-and #(5) g7(w5, sel1, sel_not0);
-and #(5) g8(w6, w5, in2); //W6
+and #(5) g7 (w5,sel1,sel_not0);
+and #(5) g8 (w6,w5,in2); //W6
 
-and #(5) g9(w7, sel1, sel0);
-and #(5) g10(w8, in3, w7);
+and #(5) g9 (w7,sel1,sel0);
+and #(5) g10 (w8,in3,w7);
 
 endmodule
