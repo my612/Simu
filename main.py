0dfd4bfc1fa90433e099324ba291629c00b8b585
ebtsamzyada <ebtsamzyada@aucegypt.edu>
2024-10-24 00:11:15 +0300

    .

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 from utils.reading import parseVerilog
 from utils.structures import Change
-from utils.utils import printPath, simulate
-from utils.reading import parsestimuli
+from utils.utils import printPath, simulatePath
 
 
 def main():
     ins, outs, gates, inputs = parseVerilog("./tests/circ1.v")
-    simulate('in0', inputs, outs, gates, Change('in0', 1))
-    ins, outs, gates, inputs = parseVerilog("./tests/circ1.v")
-    simulate('in0', inputs, outs, gates, Change('in0', 1))
-    timess, inputNamess, inputValuess, ins = parsestimuli("./utils/file.stim")
-    print("Times:", timess)
-    print("Input Names:", inputNamess)
-    print("Input Values:", inputValuess)    
-    print("Instructions", ins)
+    simulatePath('in0', inputs, outs, gates, Change('in0', 1))
     
 if __name__ == "__main__":
     main()
