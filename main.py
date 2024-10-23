97ff1c058299e4ca52b8046666551e19569f1612
my612 <muhammadyasser@aucegypt.edu>
2024-10-23 11:17:06 +0300

    nothing big

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from utils.reading import parseVerilog
 from utils.utils import printPath
 
+
 def main():
     ins, outs, gates, inputs = parseVerilog("./tests/circ1.v")
-    printPath('sel0', inputs, outs, gates)
+    printPath("sel0", inputs, outs, gates)
 
 
 if __name__ == "__main__":
