9a77e4735c2b75083f216868d5e15f53ede47dbe
Omar <omarabdelhady@aucegypt.edu>
2024-10-31 20:10:53 +0300

    .

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from utils.reading import parseVerilog, parseStimuli
 
 def main():
     ins, outs, gates, inputs = parseVerilog(
-        "/mnt/nvme0n1p4/DD1 Project 1/DD1-project1/tests/circ6.v"
+        "tests\circ6.v"
     )
     instructions = parseStimuli("./utils/file.stim")
     simulate(instructions, inputs, outs, gates)
