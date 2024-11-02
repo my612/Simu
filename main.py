7949be075791109854f8ac3806c8c41b96339c59
ebtsamzyada <ebtsamzyada@aucegypt.edu>
2024-11-02 23:07:27 +0200

    merged all into main

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ from utils.structures import Change
 from utils.utils import simulatePath, simulate
 from utils.reading import parseStimuli
 from utils.waveform import waveform
+import simulator_GUI
+import tkinter as tk
 
 
 #TODO adjust for the case when there's a space between the parameters.
 #TODO Find a way to pass the output into another gate
 #TODO CLEAN THE CODE (Unwanted comments, print statements, etc)
 def main():
+    
+    root = tk.Tk()
+    app = simulator_GUI(root)
+    root.mainloop()
     ins, outs, gates, inputs = parseVerilog(
         "./tests/circ3.v"   
     )
