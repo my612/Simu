7aaaa4a2aacfd2d7b4270f70b1f5584a267d6c1d
my612 <muhammadyasser@aucegypt.edu>
2024-11-02 23:39:03 +0200

    changed structure

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from utils.structures import Change
 from utils.utils import simulatePath, simulate
 from utils.reading import parseStimuli
 from utils.waveform import waveform
-import simulator_GUI
+import utils.simulator_GUI as simulator_GUI
 import tkinter as tk
 
 
-from simulator_GUI import simulator_GUI
+from utils.simulator_GUI import simulator_GUI
 import tkinter as tk
 def main():
     root = tk.Tk()
