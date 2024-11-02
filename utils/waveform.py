bd700807499605a3ce801af613d580b94091bd00
ebtsamzyada <ebtsamzyada@aucegypt.edu>
2024-11-03 01:48:51 +0200

    added comments to the waveform file

--- utils/waveform.py
+++ utils/waveform.py
@@ -2,21 +2,21 @@ from matplotlib import pyplot as plt
 import numpy as np
 from utils.reading import parseSimFile
 
-
-def waveform(simPath, inputs, outputs):
-    data = parseSimFile(simPath, inputs, outputs)
+# Function to plot the waveform
+def waveform(simPath, inputs, outputs): 
+    data = parseSimFile(simPath, inputs, outputs) # Parsing the simulation file
     maxTime = 0
-    keysLen = data.keys().__len__()
-    fig, ax = plt.subplots(keysLen, sharex=True, sharey=True)
-    fig.set_layout_engine("constrained")
-    fig.subplots_adjust(left=2, right=2, top=2, bottom=2)
-    for i, key in enumerate(data.keys()):
-        x_values, y_values = zip(*[(int(x), int(y)) for x, y in data[key]])
-        maxTime = max(maxTime, max(x_values))
-        x_values = list(x_values) + [maxTime, maxTime * 1.5]
-        y_values = list(y_values) + [y_values[-1], y_values[-1]]
-        ax[i].step(x_values, y_values, where="post")
-        ax[i].text(
+    keysLen = data.keys().__len__()  # Getting the number of keys
+    fig, ax = plt.subplots(keysLen, sharex=True, sharey=True) # Creating the subplots
+    fig.set_layout_engine("constrained") # Setting the layout engine
+    fig.subplots_adjust(left=2, right=2, top=2, bottom=2) 
+    for i, key in enumerate(data.keys()): # Iterating through the keys to plot the waveforms for each key
+        x_values, y_values = zip(*[(int(x), int(y)) for x, y in data[key]]) # Getting the x and y values
+        maxTime = max(maxTime, max(x_values)) # Getting the maximum time
+        x_values = list(x_values) + [maxTime, maxTime * 1.5] # Adding the maximum time to the x values
+        y_values = list(y_values) + [y_values[-1], y_values[-1]] # Adding the last y value to the y values
+        ax[i].step(x_values, y_values, where="post") # Plotting the step graph
+        ax[i].text( # Adding the text to the graph
             -0.1,
             0.5,
             key,
@@ -25,7 +25,7 @@ def waveform(simPath, inputs, outputs):
             va="center",
             ha="right",
         )
-    plt.yticks([0, 1])
-    plt.xticks(np.arange(0, maxTime * 1.5, step=100))
-    fig.set_size_inches(maxTime * 2 / 300, 10)
-    plt.show()
+    plt.yticks([0, 1])  # Setting the y ticks
+    plt.xticks(np.arange(0, maxTime * 1.5, step=100)) # Setting the x ticks
+    fig.set_size_inches(maxTime * 2 / 300, 10) # Setting the size of the figure
+    plt.show() 
